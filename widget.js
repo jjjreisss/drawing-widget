3f540d0617dd4ea3d32155345344a8ca41d96e7e
Jonathan Reis <jon.michel.reis@gmail.com>
2016-02-19 20:45:17 -0800

    back to nrmal

--- widget.js
+++ widget.js
@@ -211,91 +211,15 @@
     this.history = [this.getImageData(), this.getImageData(), this.getImageData(), this.getImageData(), this.getImageData()];
   };
 
-  var StampCanvas = function(id, width, height) {
-    this.canvas = document.getElementById(id);
-    this.ctx = this.canvas.getContext('2d');
-    this.canvas.width = width;
-    this.canvas.height = height;
-    this.img = new Image();
-    this.scale = 1;
-  };
-
-  StampCanvas.prototype.width = function () {
-    return this.canvas.width;
-  };
-
-  StampCanvas.prototype.height = function () {
-    return this.canvas.height;
-  };
-
-  StampCanvas.prototype.loadImage = function (url) {
-    this.img = new Image();
-    this.img.crossOrigin="anonymous";
-    this.img.src = url;
-    this.img.onload = function() {
-      this.ctx.drawImage(this.img, 0, 0, 500, 500, 0, 0, this.width(), this.height());
-    }.bind(this);
-  };
-
-  StampCanvas.prototype.getImageData = function () {
-    return this.ctx.getImageData(0,0,this.width(),this.height());
-  };
-
-  StampCanvas.prototype.putImageData = function (imageData) {
-    this.clear();
-    this.ctx.putImageData(imageData, 0, 0);
-  };
-
-  StampCanvas.prototype.toData = function () {
-    return this.canvas.toDataURL("image/png");
-  };
-
-  StampCanvas.prototype.clear = function () {
-    this.ctx.clearRect(0, 0, this.width(), this.height());
-  };
-
-  StampCanvas.prototype.scaleUp = function () {
-    this.scale = this.scale * 1.2;
-    var newWidth = 150 * this.scale;
-    var newHeight = 150 * this.scale;
-    this.canvas.width = newWidth;
-    this.canvas.height = newHeight;
-
-    this.ctx.drawImage(
-      this.img, 0, 0, 500, 500,
-                0, 0, newWidth, newHeight
-              );
-  };
-
-  StampCanvas.prototype.scaleDown = function () {
-    this.scale = this.scale / 1.2;
-    var newWidth = 150 * this.scale;
-    var newHeight = 150 * this.scale;
-    this.canvas.width = newWidth;
-    this.canvas.height = newHeight;
-
-    this.ctx.drawImage(
-      this.img, 0, 0, 500, 500,
-                0, 0, newWidth, newHeight
-              );
-  };
-
-
 
     /******** Our main function ********/
   function main() {
-    var drawingWidgetElement = document.getElementById("drawing-widget");
-    var drawingCanvasElement = document.createElement("canvas");
-    drawingCanvasElement.style.width = drawingWidgetElement.style.width;
-    drawingCanvasElement.style.height = drawingWidgetElement.style.height;
-    drawingCanvasElement.id = "drawing-canvas";
-    drawingWidgetElement.appendChild(drawingCanvasElement);
-    var drawingCanvas = new DrawingCanvas("drawing-canvas");
-
-    drawingCanvasElement.addEventListener("mousedown", drawingCanvas.mouseDown.bind(drawingCanvas), true);
-    drawingCanvasElement.addEventListener("mouseup", drawingCanvas.mouseUp.bind(drawingCanvas), true);
-    drawingCanvasElement.addEventListener("mousemove", drawingCanvas.mouseMove.bind(drawingCanvas), true);
-    drawingCanvasElement.addEventListener("mousewheel", drawingCanvas.mouseWheel.bind(drawingCanvas), true);
+    var drawingCanvas = new DrawingCanvas("drawing-canvas")
+    var canvasElement = document.getElementById("drawing-canvas");
+    canvasElement.addEventListener("mousedown", drawingCanvas.mouseDown.bind(drawingCanvas), true);
+    canvasElement.addEventListener("mouseup", drawingCanvas.mouseUp.bind(drawingCanvas), true);
+    canvasElement.addEventListener("mousemove", drawingCanvas.mouseMove.bind(drawingCanvas), true);
+    canvasElement.addEventListener("mousewheel", drawingCanvas.mouseWheel.bind(drawingCanvas), true);
 
   }
 
